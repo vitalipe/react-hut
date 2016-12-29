c362faf25004125685e9bf3649dfcac87f130602
vitali <vitali.pe@gmail.com>
2016-12-29 15:41:28 +0200

    shallow rendering

--- src/hut.js
+++ src/hut.js
@@ -1,4 +1,11 @@
 
+function isObject(val) {
+    return val != null && typeof val === 'object' && Array.isArray(val) === false;
+}
+
+function isComponentSpec(val) {
+    return (typeof val === "function" || typeof val === "string");
+}
 
 
 ReactHut.createHut = function (React, config) {
@@ -8,14 +15,55 @@ ReactHut.createHut = function (React, config) {
         throw new Error("first arg must be React!");
 
     var factory = React.createElement;
+    var isResolved = React.isValidElement;
+    var isElementOrResolved = function (val) { return isResolved(val) || isComponentSpec(val)};
+
 
     return function () {
         var args = (Array.isArray(arguments[0]) ? arguments[0] : arguments);
+        var fragment = args;
+        var resolved = [];
 
         if (!args[0])
             return null;
 
-        return factory(args[0]);
+        var i = 0;
+        while (i < fragment.length) {
+            var currentIndex = i;
+            var current = fragment[i];
+            var next = (i + 1 < fragment.length) ? fragment[i+1] : null;
+            var nextNext = (i + 2 < fragment.length) ? fragment[i+2] : null;
+
+            var props = null;
+            var children = null;
+
+
+            if (!isElementOrResolved(next)) {
+
+                if (isObject(next) || next === null)
+                    props = next;
+                else
+                    children = next;
+
+                i++;
+            }
+
+            if (!children && !isElementOrResolved(nextNext)) {
+
+                if (Array.isArray(nextNext) || fragment.length === 3) {
+                    children = nextNext;
+                    i++;
+                }
+            }
+
+            if (!isResolved(current))
+                current = fragment[currentIndex] = (factory(current, props, children));
+
+            resolved.push(current);
+            i++;
+        }
+
+        return resolved.length === 1 ? resolved[0] : resolved;
     };
 
 };
--- test/spec.js
+++ test/spec.js
@@ -23,8 +23,11 @@ describe("react-hut", () => {
         });
     });
 
-    describe("single primitive rendering", () => {
+    describe("single element rendering", () => {
         let H = reactHut.createHut(React, {});
+        let CustomElement = React.createClass({
+            render() { return <div className="custom">{this.props.value}</div>}
+        });
 
         it("should return null when called without input or with null as input", () => {
 
@@ -36,11 +39,59 @@ describe("react-hut", () => {
 
         });
 
-        it("should render a single div without props", () => {
+        it("should render a single primitive element without props", () => {
+            verifyTree(H("div"), <div/>);
+            verifyTree(H("div", {}), <div/>);
+            verifyTree(H("div", {}, []), <div/>);
 
-            verifyTree(H("div"), <div></div>);
+            verifyTree(H(CustomElement), <CustomElement/>);
+            verifyTree(H(CustomElement, {}), <CustomElement/>);
+            verifyTree(H(CustomElement, {}, []), <CustomElement/>);
 
         });
 
+        it("should render any primitive element", () => {
+            verifyTree(H("div"), <div/>);
+            verifyTree(H("i"), <i/>);
+            verifyTree(H("button"), <button/>);
+            verifyTree(H("h1"), <h1/>);
+        });
+
+        it("should render a single element with props", () => {
+            verifyTree(H(CustomElement, {value : "demo"}, []), <CustomElement value="demo" />);
+            verifyTree(H("div", {className : "demo"}, []), <div className="demo" />);
+            verifyTree(H("div", {className : "demo"}), <div className="demo"/>);
+        });
+
+
+        it("should accept a single element in an array", () => {
+            verifyTree(H(["div", {className : "demo"}]), <div className="demo"></div>);
+            verifyTree(H(["div"]), <div/>);
+        });
+
+        it("should teat functions or strings in the same array as sibling elements", () => {
+            verifyTree(H(["div", "i", "h1", CustomElement]), [<div/>, <i/>, <h1 />, <CustomElement/>]);
+        });
+
+
+        it("should return a single element when there is only one top component ", () => {
+            assert.isNotArray(H(["div"]));
+            assert.isNotArray(H(["div", {}]));
+            assert.isNotArray(H(["div", {}, [] ]));
+
+            assert.isNotArray(H("div"));
+            assert.isNotArray(H("div", {}));
+            assert.isNotArray(H("div", {}, [] ));
+        });
+
     });
+
+    /**
+    describe("element with children rendering", () => {
+
+        it("should render element with props & a single child", () => {
+            verifyTree(H(["div", {}, ["div"]]), <div className="demo"><div></div></div>);
+        });
+
+    }); **/
 });
